3ae1eea59e27a9a1ada4809e09a1c55cb7670a73
Ang Hao Yang <yan9za1@gmail.com>
2022-01-02 02:04:39 +0800

    Replace currying and and uncurrying functions with dynamic implementations.

--- src/Data/Function/Uncurried.js
+++ src/Data/Function/Uncurried.js
@@ -2,219 +2,59 @@
 
 // module Data.Function.Uncurried
 
+var mkFn = function (f) {
+  return function () {
+    return Array.prototype.slice.call(arguments)
+      .reduce(function (ff, a) { return ff(a); }, f);
+  };
+};
+
+var runFn = function (l, f) {
+  return function (a) {
+    return l <= 1 ? f(a) : runFn(l - 1, f.bind(null, a));
+  };
+};
+
 exports.mkFn0 = function (fn) {
   return function () {
     return fn();
   };
 };
 
-exports.mkFn2 = function (fn) {
-  /* jshint maxparams: 2 */
-  return function (a, b) {
-    return fn(a)(b);
-  };
-};
+exports.mkFn2 = mkFn;
 
-exports.mkFn3 = function (fn) {
-  /* jshint maxparams: 3 */
-  return function (a, b, c) {
-    return fn(a)(b)(c);
-  };
-};
+exports.mkFn3 = mkFn;
 
-exports.mkFn4 = function (fn) {
-  /* jshint maxparams: 4 */
-  return function (a, b, c, d) {
-    return fn(a)(b)(c)(d);
-  };
-};
+exports.mkFn4 = mkFn;
 
-exports.mkFn5 = function (fn) {
-  /* jshint maxparams: 5 */
-  return function (a, b, c, d, e) {
-    return fn(a)(b)(c)(d)(e);
-  };
-};
+exports.mkFn5 = mkFn;
 
-exports.mkFn6 = function (fn) {
-  /* jshint maxparams: 6 */
-  return function (a, b, c, d, e, f) {
-    return fn(a)(b)(c)(d)(e)(f);
-  };
-};
+exports.mkFn6 = mkFn;
 
-exports.mkFn7 = function (fn) {
-  /* jshint maxparams: 7 */
-  return function (a, b, c, d, e, f, g) {
-    return fn(a)(b)(c)(d)(e)(f)(g);
-  };
-};
+exports.mkFn7 = mkFn;
 
-exports.mkFn8 = function (fn) {
-  /* jshint maxparams: 8 */
-  return function (a, b, c, d, e, f, g, h) {
-    return fn(a)(b)(c)(d)(e)(f)(g)(h);
-  };
-};
+exports.mkFn8 = mkFn;
 
-exports.mkFn9 = function (fn) {
-  /* jshint maxparams: 9 */
-  return function (a, b, c, d, e, f, g, h, i) {
-    return fn(a)(b)(c)(d)(e)(f)(g)(h)(i);
-  };
-};
+exports.mkFn9 = mkFn;
 
-exports.mkFn10 = function (fn) {
-  /* jshint maxparams: 10 */
-  return function (a, b, c, d, e, f, g, h, i, j) {
-    return fn(a)(b)(c)(d)(e)(f)(g)(h)(i)(j);
-  };
-};
+exports.mkFn10 = mkFn;
 
-exports.runFn0 = function (fn) {
-  return fn();
-};
+exports.runFn0 = runFn.bind(null, 0);
 
-exports.runFn2 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return fn(a, b);
-    };
-  };
-};
+exports.runFn2 = runFn.bind(null, 2);
 
-exports.runFn3 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return fn(a, b, c);
-      };
-    };
-  };
-};
+exports.runFn3 = runFn.bind(null, 3);
 
-exports.runFn4 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return function (d) {
-          return fn(a, b, c, d);
-        };
-      };
-    };
-  };
-};
+exports.runFn4 = runFn.bind(null, 4);
 
-exports.runFn5 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return function (d) {
-          return function (e) {
-            return fn(a, b, c, d, e);
-          };
-        };
-      };
-    };
-  };
-};
+exports.runFn5 = runFn.bind(null, 5);
 
-exports.runFn6 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return function (d) {
-          return function (e) {
-            return function (f) {
-              return fn(a, b, c, d, e, f);
-            };
-          };
-        };
-      };
-    };
-  };
-};
+exports.runFn6 = runFn.bind(null, 6);
 
-exports.runFn7 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return function (d) {
-          return function (e) {
-            return function (f) {
-              return function (g) {
-                return fn(a, b, c, d, e, f, g);
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-};
+exports.runFn7 = runFn.bind(null, 7);
 
-exports.runFn8 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return function (d) {
-          return function (e) {
-            return function (f) {
-              return function (g) {
-                return function (h) {
-                  return fn(a, b, c, d, e, f, g, h);
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-};
+exports.runFn8 = runFn.bind(null, 8);
 
-exports.runFn9 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return function (d) {
-          return function (e) {
-            return function (f) {
-              return function (g) {
-                return function (h) {
-                  return function (i) {
-                    return fn(a, b, c, d, e, f, g, h, i);
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-};
+exports.runFn9 = runFn.bind(null, 9);
 
-exports.runFn10 = function (fn) {
-  return function (a) {
-    return function (b) {
-      return function (c) {
-        return function (d) {
-          return function (e) {
-            return function (f) {
-              return function (g) {
-                return function (h) {
-                  return function (i) {
-                    return function (j) {
-                      return fn(a, b, c, d, e, f, g, h, i, j);
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-};
+exports.runFn10 = runFn.bind(null, 10);
